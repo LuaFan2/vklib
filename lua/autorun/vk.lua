547f6b4ae254353c374e1acd153598737221ef5c
LuaFan2 <51294434+LuaFan2@users.noreply.github.com>
2020-09-27 21:23:38 +0500

    Session version support & stylistics

--- lua/autorun/vk.lua
+++ lua/autorun/vk.lua
@@ -1,7 +1,7 @@
 vk = {}
 
 --
--- Config zone
+-- Internal
 --
 
 vk.Version = '5.103'
@@ -11,69 +11,70 @@ vk.Version = '5.103'
 --
 
 local function _newStack ()
-    return {''}   -- starts with an empty string
+	return {''}   -- starts with an empty string
 end
-    
+	
 local function _addString (stack, s)
-    table.insert(stack, s)    -- push 's' into the the stack
-    for i=table.getn(stack)-1, 1, -1 do
-        if string.len(stack[i]) > string.len(stack[i+1]) then
-            break
-        end
-        stack[i] = stack[i] .. table.remove(stack)
-    end
+	table.insert(stack, s)    -- push 's' into the the stack
+	for i=table.getn(stack)-1, 1, -1 do
+		if string.len(stack[i]) > string.len(stack[i+1]) then
+			break
+		end
+		stack[i] = stack[i] .. table.remove(stack)
+	end
 end
 
 local function _toString(t)
-    return table.concat(t)
+	return table.concat(t)
 end
 
 --
 --
 
 function vk:Session(token, options)
-    local obj = {}
-        obj.token = token
-        obj.options = options or {}
-    
-    local api = setmetatable({}, {__index = function(t, kg)
-        local group, method, result
-        local argList = _newStack()
-        
-        group = kg
-        kg = setmetatable({}, {__index = function(_, km)
-            method = km
-          
-            return setmetatable({}, {__call = function(s, f)
-              return {cb = function(this, callb)
-                callb = callb or function(...) end
-                if not f.v then f.v = vk.Version end
+	local obj = {}
+		obj.token = token
+		obj.options = options or {}
+	
+	local api = setmetatable({}, {__index = function(t, kg)
+		local group, method, result
+		local argList = _newStack()
+		
+		group = kg
+		kg = setmetatable({}, {__index = function(_, km)
+			method = km
+		  
+			return setmetatable({}, {__call = function(s, f)
+				return {cb = function(this, callb)
+					callb = callb or function(...) end
+					if not f.v then f.v = (obj.options.version and obj.options.version) or vk.Version end
 
-                for k, v in pairs(f) do
-                  _addString(argList, k .. '=' .. v .. '&')
-                end
-                
-                local req = string.gsub('https://api.vk.com/method/' .. group .. '.' .. method .. '?' .. _toString(argList) .. 'access_token=' .. obj.token, "%s+", "%%20")
-                http.Fetch(req, 
-                    function(body)
-                        result = obj.options.raw and body or util.JSONToTable(body)
-                        
-                        callb(result)
-                    end,
-                    function(error)
-                        callb(error)
-                end)
+					for k, v in pairs(f) do
+					  _addString(argList, k .. '=' .. v .. '&')
+					end
+					
+					local req = string.gsub('https://api.vk.com/method/' .. group .. '.' .. method .. '?' .. _toString(argList) .. 'access_token=' .. obj.token, "%s+", "%%20")
+					http.Fetch(req, 
+						function(body)
+							result = obj.options.raw and body or util.JSONToTable(body)
+							
+							callb(result)
+						end,
+						function(error)
+							callb(error)
+						end
+					)
 
-                return true
-              end}
-            end})
-        end})
-        
-        return kg
-    end})
+					return true
+			  	end}
+			end})
+		end})
+		
+		return kg
+	end})
 
-    setmetatable(obj, self)
-    self.__index = self
-    
-    return api
+	setmetatable(obj, self)
+	self.__index = self
+	
+	return api
 end
